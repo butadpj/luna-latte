fe9afe72c58343bf1303df09ad9ccaf81bd5fe34
butadpj <butadpj@gmail.com>
2024-01-14 15:31:17 +0800

    test webhook

--- app/api/messenger-webhook/route.ts
+++ app/api/messenger-webhook/route.ts
@@ -1,4 +1,26 @@
-import { NextRequest } from "next/server";
+//@ts-nocheck
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === "sample") {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+}
 
 export async function POST(req: NextRequest) {
   let body = req.body;
