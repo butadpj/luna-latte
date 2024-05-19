99b53097bf2d2d6223318fca803992296d2d403e
butadpj <butadpj@gmail.com>
2024-05-19 11:44:29 +0800

    [Update guarddialog]: Add tiktok link

--- shared/GuardDialog.tsx
+++ shared/GuardDialog.tsx
@@ -11,6 +11,11 @@ import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import SendToMessenger from "./SendToMessenger";
 import { DialogDescription } from "@radix-ui/react-dialog";
+import Image from "next/image";
+import { Button, buttonVariants } from "@/shared/ui/button";
+import { Separator } from "@/shared/ui/separator";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 
 export default function GuardDialog() {
   const [isClient, setIsClient] = useState(false);
@@ -63,6 +68,35 @@ export default function GuardDialog() {
               setIsOptInDone(true);
             }}
           />
+          <Separator />
+          <DialogDescription className="text-center text-sm gap-1 flex flex-wrap items-center justify-center">
+            or watch our journey here:{" "}
+            <Link
+              target="_blank"
+              href={"https://www.tiktok.com/@lunalatteph"}
+              className={twMerge(
+                buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                }),
+                "flex gap-1 items-center"
+              )}
+            >
+              <span>Tiktok: </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="14"
+                width="12.25"
+                viewBox="0 0 448 512"
+              >
+                {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                <path
+                  fill="var(--foreground)"
+                  d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"
+                />
+              </svg>
+            </Link>{" "}
+          </DialogDescription>
         </>
       );
     }
